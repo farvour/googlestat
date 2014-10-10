87c2687341485e23719189603a5013bf682fa8ec
Tom Farvour <tom@farvour.com>
2014-10-09 20:34:49 -0500

    Create the ping model and script to initialize it in the database.

--- googlestat/models.py
+++ googlestat/models.py
@@ -1,27 +1,20 @@
-from sqlalchemy import (
-    Column,
-    Index,
-    Integer,
-    Text,
-    )
-
+""" Monitoring application models.
+"""
+from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-    )
-
+from sqlalchemy.orm import scoped_session, sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+class PingModel(Base):
+    """ Defines a model for storing pings to the site.
+    """
+    __tablename__ = 'pings'
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+    id = Column(Integer, primary_key=True)
+    status_code = Column(Integer, nullable=False, index=True)
+    response_time = Column(Integer, nullable=False)
+    date_added = Column(DateTime, nullable=False)
--- googlestat/scripts/initializedb.py
+++ googlestat/scripts/initializedb.py
@@ -1,33 +1,25 @@
+""" Initialize the database for storing data.
+"""
 import os
 import sys
-import transaction
-
 from sqlalchemy import engine_from_config
-
-from pyramid.paster import (
-    get_appsettings,
-    setup_logging,
-    )
-
+from pyramid.paster import get_appsettings, setup_logging
 from pyramid.scripts.common import parse_vars
-
-from ..models import (
-    DBSession,
-    MyModel,
-    Base,
-    )
+from ..models import DBSession, Base
 
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
-    print('usage: %s <config_uri> [var=value]\n'
-          '(example: "%s development.ini")' % (cmd, cmd))
+
+    print('usage: %s <config_uri> [var=value]\n(example: "%s development.ini")' % (cmd, cmd))
+
     sys.exit(1)
 
 
 def main(argv=sys.argv):
     if len(argv) < 2:
         usage(argv)
+
     config_uri = argv[1]
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
@@ -35,6 +27,3 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
